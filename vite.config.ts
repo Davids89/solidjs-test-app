bd631b0f250d2b90dff93c781c8095978d224d81
David Luque <david.luque@flywire.com>
2023-03-11 19:21:27 +0100

    [feature] Configure resolve alias for Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import * as path from 'path';
 
 export default defineConfig({
   plugins: [solidPlugin()],
@@ -9,5 +10,8 @@ export default defineConfig({
   },
   build: {
     target: 'esnext',
+  },
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
 });
